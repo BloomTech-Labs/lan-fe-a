5797d976eb2b8f82ae5ccab9d048b44ea1d24030
Miguel Nicolas <miguelqnicolas@gmail.com>
2021-01-01 23:18:30 -0500

    Update landing page

--- src/components/common/landing.js
+++ src/components/common/landing.js
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from 'react';
 // import { Link } from 'react-router-dom';
 import Loader from './loader';
 import LandingContainer from './styles/landingStyle';
-import viewee from '../../img/viewee.png';
+import lambdaschool from '../../img/lambda-school.png';
 import google from '../../img/google.png';
-import facebook from '../../img/facebook.png';
-import twitter from '../../img/twitter.png';
+// import facebook from '../../img/facebook.png';
+// import twitter from '../../img/twitter.png';
 
 const BACKEND_URL = process.env.REACT_APP_DEPLOYED_URL || 'http://localhost:5000';
 
@@ -25,10 +25,11 @@ const Landing = props => {
                     <div className='left-section-container'>
                         <div className='left-section'>
                             <div className='logo-and-name'>
-                                <img src={viewee} alt='viewee logo' />
-                                <h1>viewee</h1>
+                                <img src={lambdaschool} alt='viewee logo' />
+                                <h1>Lambda Alumni Network</h1>
                             </div>
-                            <p className='description'>Prepare for tech interviews with viewee! Browse an array of questions fellow Lambda School students have received during interviews, view input from career coaches and how other students would answer, and post questions you have received and how you responded for feedback.</p>
+                            {/* <p className='description'>Prepare for tech interviews with viewee! Browse an array of questions fellow Lambda School students have received during interviews, view input from career coaches and how other students would answer, and post questions you have received and how you responded for feedback.</p> */}
+                            <p className='description'>A community platform for Lambda School alumni</p>
                         </div>
                     </div>
                     <div className='right-section-container'>
@@ -40,7 +41,7 @@ const Landing = props => {
                                     Continue with Google
                                 </button>
                             </a>
-                            <a className='social-media-link' href={`${BACKEND_URL}/api/auth/facebook`} onClick={() => setFetching(true)}>
+                            {/* <a className='social-media-link' href={`${BACKEND_URL}/api/auth/facebook`} onClick={() => setFetching(true)}>
                                 <button className='facebook-button'>
                                     <img src={facebook} alt='facebook logo' />
                                     Continue with Facebook
@@ -51,7 +52,7 @@ const Landing = props => {
                                     <img src={twitter} alt='twitter logo' />
                                     Continue with Twitter
                                 </button>
-                            </a>
+                            </a> */}
                             <p className='terms'>By continuing, you agree to viewee's Terms and Privacy Policy.</p>
                             {/* <p className='terms'>By continuing, you agree to our <Link to='/termsofservice'>Terms of Service</Link> and <Link to='/privacypolicy'>Privacy Policy</Link>.</p> */}
                             {/* <p className='instead'>Register with a username and password <Link to='/register'>instead</Link>.</p> */}
