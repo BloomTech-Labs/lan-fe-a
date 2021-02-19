5e7640d66293f817160fa0a310cf5411e810e155
James <info@thecodediver.com>
2021-02-19 09:30:21 -0500

    adds styling to roomDashboard component including sidebar and posts in a room

--- src/components/common/sidebar.js
+++ src/components/common/sidebar.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledSidebar = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 5% 10px 5% 30px;
+    height: 80vh;
+    justify-content: space-around;
+    a {
+        color: white;
+        text-decoration: none;
+        font-size: 20px;
+    }
+    .active-sidebar-room {
+        color: black;
+        background-color: white;
+        
+    }
+`
 
 const Sidebar = (props) => {
   return (
-    <div>
+    <StyledSidebar>
       {props.rooms.map((room, index) => {
         return (
-          <Link to={`/room/${room.id}`} key={index}>
+          <NavLink activeClassName="active-sidebar-room" to={`/room/${room.id}`} key={index}>
             {room.room_name}
-          </Link>
+          </NavLink>
         );
       })}
-    </div>
+    </StyledSidebar>
   );
 };
 
