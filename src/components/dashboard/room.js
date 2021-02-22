dbc86acd7047744dcf8eaa9e05822d3b2c5597b5
James <info@thecodediver.com>
2021-02-22 08:08:48 -0500

    updates room component to fix merge conflicts with styling

--- src/components/dashboard/room.js
+++ src/components/dashboard/room.js
@@ -4,77 +4,109 @@ import { Link } from 'react-router-dom'
 import styled from 'styled-components';
 import arrow from '../../img/arrow.png';
 import returnpointer from '../../img/return.png';
+import likeicon from "../../img/likeicon.png";
+import replyicon from "../../img/replyicon.png";
 import Modal from 'react-modal';
 import CreatePostContainer from '../post/styles/createPostStyle'
 import { postQuestion, fetchPostByRoom } from '../../actions'
 
 const StyledRoomContainer = styled.div`
-    width : 90%;
-    padding: 2%;
-    
-    .single-room-name {
-        color: white;
-    }
-`
+  width : 90%;
+  padding: 2%;
+
+  .single-room-name {
+    color: white;
+  }
+`;
 
 const StyledPost = styled.div`
+  color: white;
+  border: 2px solid #272626;
+  border-radius: 25px;
+  padding: 2%;
+  margin: 2.2% 0%;
+  background-color: #141414;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  justify-content: space-evenly;
+  a {
+    text-decoration: none;
     color: white;
-    border: 2px solid grey;
-    padding: 2px;
-    margin: 7px 0px;
-    box-shadow: 5px solid black;
-    a {
-        text-decoration: none;
-        color: white;
-        transition: all .2s;
-        &:hover {
-            color: grey;
-        }
+    transition: all .2s;
+      &:hover {
+        color: grey;
+      }
     }
-    .profile-img {
-        border-radius: 50%;
-        width: 40px;
-        height: 40px;
-    }
-    .single-post-footer {
-        display: flex;
-        justify-content: space-between;
-    }
- 
-`
-const StyledPointer = styled.div`
-    display: flex; 
-    justify-content: space-between;
+  h4 {
+    text-transform: capitalize;
+    margin-left: 1%;
+    font-size: 1.4rem;
+  }
+  h3 {
+    margin-bottom: 0.5%;
+    font-size: 1.25rem;
+  }
+  .profile {
+    display: flex;
     align-items: center;
-    background-color: #141414;
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2),
-    0 6px 20px 0 rgba(0, 0, 0, 0.19);
-    border-radius: 20px;
-    .single-room-navigation {
-        display: flex;
-        align-items: center;
-    }
-    .return-pointer{
-      margin: 20px;
-      padding: 7px;
-      border: 1px solid white;
-      border-radius: 15%;      
-    }
-    .single-room-name{
-      margin-top: 1.5%;
-      margin-left: 1.2%
-    }
-    .create-post-button {
-        font-size: 16px;
-        color: white;
-        background-color: #0D0D0D;
-        padding: 10px;
-        border: 1px solid white;
-        border-radius: 5px;
-        height: 40px;
-        cursor: pointer; 
-    }
-`
+    margin-bottom: 1%;
+    font-weight: lighter;
+  }
+  .profile-img {
+    border-radius: 50%;
+    width: 40px;
+    height: 40px;
+  }
+  .single-post-footer {
+    display: flex;
+    justify-content: flex-end;
+    margin-top: 0.8%;
+    align-items: center;
+  }
+  span {
+    display: flex;
+    margin-right: 2%;
+  }
+  p {
+    justify-self: stretch;
+    color: #e0dcdc;
+    font-size: 1.1rem;
+  }
+`;
+const StyledPointer = styled.div`
+display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #141414;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  border-radius: 20px;
+  h1 {
+    font-size: 1.8rem;
+  }
+  .return-pointer {
+    margin: 20px;
+    padding: 7px;
+    border: 1px solid white;
+    border-radius: 15%;
+  }
+  .single-room-name {
+    margin-top: 1.5%;
+    margin-left: 1.2%;
+  }
+  .single-room-navigation {
+    display: flex;
+    align-items: center;
+  }
+  .create-post-button {
+    font-size: 16px;
+    color: white;
+    background-color: #0D0D0D;
+    padding: 10px;
+    border: 1px solid white;
+    border-radius: 5px;
+    height: 40px;
+    cursor: pointer; 
+  }
+`;
 
 const customStyles = {
     content: {
@@ -86,7 +118,7 @@ const customStyles = {
       transform: 'translate(-50%, -50%)',
       border: 'none',
       backgroundColor: '#0D0D0D',
-      color: '#333',
+      color: 'white',
       padding: '96px 64px',
     },
     overlay: {
@@ -174,12 +206,32 @@ const Room = (props) => {
             <StyledPost className="post_card" key={index}>
               <Link to={`/post/${post.id}`}>
                 <div className="profile">
-                    <img className="profile-img" src={post.profile_picture} />
-                    <p>{post.display_name}</p>
+                  <img className="profile-img" src={post.profile_picture} />
+                  <h4>{post.display_name}</h4>
                 </div>
                 <h3> {post.title} </h3>
                 <p> {post.description} </p>
-                <p class="single-post-footer" onClick={() => console.log(`click ${post.id}`) }><span>Likes: {post.likes}</span><span>Comments: {post.comments}</span></p>
+                <p
+                  class="single-post-footer"
+                  onClick={() => console.log(`click ${post.id}`)}
+                >
+                  <span>
+                    <img
+                      className="white-like-icon"
+                      src={likeicon}
+                      alt="like-icon"
+                    />{" "}
+                    {post.likes}
+                  </span>
+                  <span>
+                    <img
+                      className="white-reply-icon"
+                      src={replyicon}
+                      alt="reply-icon"
+                    />{" "}
+                    {post.comments}
+                  </span>
+                </p>
               </Link>
             </StyledPost>
           </>
