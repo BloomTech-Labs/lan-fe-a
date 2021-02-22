527fdf759e97711af892fef26657f5b0db168941
James <info@thecodediver.com>
2021-02-21 20:05:15 -0500

    adds modal to create posts in room component

--- src/components/dashboard/room.js
+++ src/components/dashboard/room.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom'
 import styled from 'styled-components';
 import arrow from '../../img/arrow.png';
 import returnpointer from '../../img/return.png';
+import Modal from 'react-modal';
+import CreatePostContainer from '../post/styles/createPostStyle'
+import { postQuestion, fetchPostByRoom } from '../../actions'
 
 const StyledRoomContainer = styled.div`
     width : 90%;
@@ -42,11 +45,15 @@ const StyledPost = styled.div`
 const StyledPointer = styled.div`
     display: flex; 
     justify-content: space-between;
-    align-item: center;
+    align-items: center;
     background-color: #141414;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2),
     0 6px 20px 0 rgba(0, 0, 0, 0.19);
     border-radius: 20px;
+    .single-room-navigation {
+        display: flex;
+        align-items: center;
+    }
     .return-pointer{
       margin: 20px;
       padding: 7px;
@@ -57,16 +64,109 @@ const StyledPointer = styled.div`
       margin-top: 1.5%;
       margin-left: 1.2%
     }
+    .create-post-button {
+        font-size: 16px;
+        color: white;
+        background-color: #0D0D0D;
+        padding: 10px;
+        border: 1px solid white;
+        border-radius: 5px;
+        height: 40px;
+        cursor: pointer; 
+    }
 `
 
+const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+      border: 'none',
+      backgroundColor: '#0D0D0D',
+      color: '#333',
+      padding: '96px 64px',
+    },
+    overlay: {
+      backgroundColor: 'rgba(0, 0, 0, 0.2)',
+    },
+};
+
+
 const Room = (props) => {
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+  const [input, setInput] = useState({
+    title: '',
+    description: ''
+  });
+  const [error, setError] = useState({
+    checkbox: '',
+    title: '',
+    description: ''
+  });
+
+  const onChange = event => {
+    setInput({
+        ...input,
+        [event.target.name]: event.target.value
+    });
+  };
+
+  const onSubmit = event => {
+    event.preventDefault();
+    if (input.title === '') {
+        setError({
+            title: 'Please enter a title',
+            description: ''
+        });
+    } else if (input.description === '') {
+        setError({
+            title: '',
+            description: 'Please enter post content'
+        });
+    } else {
+        setError({
+            title: '',
+            description: ''
+        });
+        props.postQuestion(input.title, input.description, props.id, props.history)
+            .then(response => {
+                setInput({
+                    title: '',
+                    description: ''
+                })
+                props.fetchPostByRoom(props.id)
+                closeModal()
+            })
+            .catch(error => {
+                console.log(error);
+                setError({
+                    title: '',
+                    description: 'Unable to create post please try again'
+                });
+            });
+    };
+  };
   return (
     <StyledRoomContainer>
-      {console.log(props.rooms)} 
       <StyledPointer>
-        {props.rooms.filter(item => item.id == props.id).map(item =>   <h1 className="single-room-name"># {item.room_name}</h1>)}
+        {props.rooms.filter(item => item.id == props.id).map(item => <h1 className="single-room-name"># {item.room_name}</h1>)}
         {/* add return pointer to go back previous page */}
-        <img src={returnpointer} className="return-pointer" alt="return-pointer"/>
+        <div className="single-room-navigation">
+            <button onClick={() => openModal()} className="create-post-button">Create Post</button>
+            <img src={returnpointer} className="return-pointer" alt="return-pointer"/>
+        </div>
+        
       </StyledPointer>
       {props.posts.map((post, index) => {
         return (
@@ -85,6 +185,32 @@ const Room = (props) => {
           </>
         );
       })}
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel='FAQ'
+        style={customStyles}
+        shouldCloseOnOverlayClick={false}>
+        <CreatePostContainer>
+            {props.rooms.filter(item => item.id == props.id).map(item => <h1 className="single-room-name"># {item.room_name}</h1>)}
+            <form autoComplete='off' spellCheck='false' onSubmit={onSubmit}>
+                {error.checkbox && <p className='error'>{error.checkbox}</p>}
+
+                <label>Title</label>
+                <input type='text' name='title' placeholder='Post Title' value={input.title} onChange={onChange} />
+                {error.title && <p className='error'>{error.title}</p>}
+
+                <label>Post Content</label>
+                <textarea type='text' name='description' placeholder='Post Content' value={input.description} onChange={onChange} />
+                {error.description && <p className='error'>{error.description}</p>}
+                
+                <div className='buttons'>
+                    <button type='button' onClick={closeModal}><i className='fas fa-times'></i>Cancel</button>
+                    <button type='submit'>Submit<i className='fas fa-check'></i></button>
+                </div>
+            </form>
+        </CreatePostContainer>
+      </Modal>
     </StyledRoomContainer>
   );
 };
@@ -96,4 +222,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(Room);
+export default connect(mapStateToProps, { postQuestion, fetchPostByRoom })(Room);
