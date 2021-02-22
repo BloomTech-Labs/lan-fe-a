042399c345e58f19080dcedde69e6a7eae3eeb3c
salza323 <sal.zamora480@gmail.com>
2021-02-22 15:55:08 -0700

    admin can now delete a user from DB.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,109 +1,106 @@
 const initialState = {
-    user: {},
-    currentUser: {},
-    rooms: [],
-    users: [],
-    posts: [],
-    currentPost: {},
-    currentPostComments: [],
-    search: '',
-    sort: '',
-    filter: '',
-    usersLikedPosts: [],
-    usersLikedComments: [],
-    individualPostIsFetching: false,
-    individualPostCommentsAreFetching: false
+  user: { role_id: 3 },
+  currentUser: {},
+  rooms: [],
+  users: [],
+  posts: [],
+  currentPost: {},
+  currentPostComments: [],
+  search: '',
+  sort: '',
+  filter: '',
+  usersLikedPosts: [],
+  usersLikedComments: [],
+  individualPostIsFetching: false,
+  individualPostCommentsAreFetching: false,
 };
 
 export const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case 'SET_USER':
-            return {
-                ...state,
-                user: action.payload
-            };
+  switch (action.type) {
+    case 'SET_USER':
+      return {
+        ...state,
+        user: action.payload,
+      };
 
-        case 'SET_ROOMS':
-            return {
-                ...state,
-                rooms: action.payload
-            }
-        
-        case 'SET_USERS':
-            return {
-                ...state,
-                users: action.payload
-            }
+    case 'SET_ROOMS':
+      return {
+        ...state,
+        rooms: action.payload,
+      };
 
-        case 'SET_POSTS':
-            return {
-                ...state,
-                posts: action.payload
-            };
+    case 'SET_USERS':
+      return {
+        ...state,
+        users: action.payload,
+      };
 
-        case 'START_FETCHING_CURRENT_POST':
-            return {
-                ...state,
-                individualPostIsFetching: true
-            };
+    case 'SET_POSTS':
+      return {
+        ...state,
+        posts: action.payload,
+      };
 
-        case 'SET_CURRENT_POST':
-            return {
-                ...state,
-                currentPost: action.payload,
-                individualPostIsFetching: false
-            };
+    case 'START_FETCHING_CURRENT_POST':
+      return {
+        ...state,
+        individualPostIsFetching: true,
+      };
 
-        case 'SET_SEARCH':
-            return {
-                ...state,
-                search: action.payload
-            };
+    case 'SET_CURRENT_POST':
+      return {
+        ...state,
+        currentPost: action.payload,
+        individualPostIsFetching: false,
+      };
 
-        case 'SET_USERS_LIKED_POSTS':
-            return {
-                ...state,
-                usersLikedPosts: action.payload
-            };
+    case 'SET_SEARCH':
+      return {
+        ...state,
+        search: action.payload,
+      };
 
-        case 'SET_POSTS_COMMENTS':
-            return {
-                ...state,
-                currentPost: {
-                    ...state.currentPost,
-                    comments: [
-                        action.payload,
-                        ...state.currentPost.comments
-                    ]
-                }
-            };
+    case 'SET_USERS_LIKED_POSTS':
+      return {
+        ...state,
+        usersLikedPosts: action.payload,
+      };
 
-        case 'SET_USERS_LIKED_COMMENTS':
-            return {
-                ...state,
-                usersLikedComments: action.payload
-            };
+    case 'SET_POSTS_COMMENTS':
+      return {
+        ...state,
+        currentPost: {
+          ...state.currentPost,
+          comments: [action.payload, ...state.currentPost.comments],
+        },
+      };
 
-        case 'SET_CURRENT_USER':
-            return {
-                ...state,
-                currentUser: action.payload
-            };
+    case 'SET_USERS_LIKED_COMMENTS':
+      return {
+        ...state,
+        usersLikedComments: action.payload,
+      };
 
-        case 'START_FETCHING_CURRENT_POST_COMMENTS':
-            return {
-                ...state,
-                individualPostCommentsAreFetching: true
-            };
+    case 'SET_CURRENT_USER':
+      return {
+        ...state,
+        currentUser: action.payload,
+      };
 
-        case 'SET_CURRENT_POST_COMMENTS':
-            return {
-                ...state,
-                currentPostComments: action.payload,
-                individualPostCommentsAreFetching: false
-            };
-        
-        default:
-            return state;
-    };
+    case 'START_FETCHING_CURRENT_POST_COMMENTS':
+      return {
+        ...state,
+        individualPostCommentsAreFetching: true,
+      };
+
+    case 'SET_CURRENT_POST_COMMENTS':
+      return {
+        ...state,
+        currentPostComments: action.payload,
+        individualPostCommentsAreFetching: false,
+      };
+
+    default:
+      return state;
+  }
 };
