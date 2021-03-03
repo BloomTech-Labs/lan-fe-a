99de5ee3f63f1f05e9c92dad1a2890dc53874d13
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2021-03-03 11:14:34 -0800

    Edit User Profile Setting page

--- src/components/user/styles/settingsStyle.js
+++ src/components/user/styles/settingsStyle.js
@@ -1,255 +1,244 @@
 import styled from 'styled-components';
 
 const SettingsContainer = styled.div`
+  width: 98%;
+  margin: 5% auto;
+  display: flex;
+  flex-wrap: wrap;
+  input {
+    background-color: #242323;
+    height: 40px;
+    width: 35%;
+    padding: 8px;
+    margin: 3% 2% 2% 2.2%;
+    border-radius: 8px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    font-family: 'Nunito', sans-serif;
+    font-size: 1rem;
+    font-weight: 500;
+    color: #ffffff;
+
+    ::placeholder {
+      color: dimgray;
+    }
+  }
+
+  button {
+    padding: 8px 12px;
+    background-color: #212529;
+    box-shadow: 2px 2px 8px #212529;
+    border: 1px solid #707b7c;
+    border-radius: 3px;
+    font-family: 'Nunito', sans-serif;
+    font-size: 0.7rem;
+    color: #ffffff;
+    cursor: pointer;
+    transition: 0.25s;
+    :hover {
+      opacity: 0.5;
+    }
+  }
+
+  h2 {
+    margin-bottom: 32px;
+    font-size: 1.75rem;
+    font-weight: 700;
+    color: #ffffff;
+  }
+
+  h3 {
     width: 98%;
-    margin: 5% auto;
+    padding: 2%;
+    font-size: 1.6rem;
+    font-weight: 500;
+    color: #ffffff;
+    border: 1px solid grey;
+    border-left-style: hidden;
+    border-right-style: hidden;
+    border-bottom-style: hidden;
+  }
+  // styling users-setting-card
+  .users-card-wrapper {
     display: flex;
     flex-wrap: wrap;
-    input{
-        background-color: #242323;
-        height: 40px;
-        width: 35%;
-        padding: 8px;
-        margin: 3% 2% 2% 2.2%;
-        border-radius: 8px;
-        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2),
-        0 6px 20px 0 rgba(0, 0, 0, 0.19);
-        font-family: "Nunito", sans-serif;
+    width: 98%;
+    padding: 3%;
+    border-top-style: hidden;
+  }
+
+  .display-name {
+    display: flex;
+    border: none;
+    margin-left: 4%;
+    .left-section {
+      p {
         font-size: 1rem;
         font-weight: 500;
         color: #ffffff;
-        /* border: 1px solid lightgrey; */
-        
-        ::placeholder {
-          color: dimgray;
+
+        :first-child {
+          margin-bottom: 4px;
+          font-weight: 600;
         }
+      }
+    }
+    button {
+      padding: 12px 26px;
+      margin-top: 16px;
+      background: linear-gradient(to right, #000000, #212121, #424949);
+      font-family: 'Nunito', sans-serif;
+      font-size: 1rem;
+      font-weight: 600;
+      color: #ffffff;
+      cursor: pointer;
+      transition: 0.25s;
+      border: 1px solid grey;
+      height: 50px;
+      /* box-shadow: 3px 3px 6px #888888; */
+      :hover {
+        opacity: 0.5;
+      }
+    }
+  }
+
+  .update {
+    padding: 16px;
+    margin-bottom: 16px;
+    background-color: #2c2f33;
+    border-radius: 3px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+
+    label {
+      margin-bottom: 4px;
+      font-size: 1rem;
+      font-weight: 500;
+      color: #ffffff;
     }
 
-    button {      
-            padding: 8px 12px;
-            background-color: #212529;
-            box-shadow: 2px 2px 8px #212529;
-            border: 1px solid #707B7C ;
-            border-radius: 3px;
-            font-family: 'Nunito', sans-serif;
-            font-size: 0.7rem;
-            color: #ffffff;
-            cursor: pointer;
-            transition: 0.25s;
-              :hover {
-                opacity: 0.5;
-              }
-          }
+    input {
+      width: 256px;
+      padding: 10px;
+      margin-bottom: 16px;
+      background-color: #23272a;
+      border: none;
+      border-radius: 3px;
+      font-family: 'Nunito', sans-serif;
+      font-size: 1rem;
+      font-weight: 500;
+      color: #ffffff;
 
-    h2 {
-        margin-bottom: 32px;
-        font-size: 1.75rem;
-        font-weight: 700;
-        color: #ffffff;
-        
+      ::placeholder {
+        color: dimgray;
+      }
     }
 
-    h3 {
-        width: 98%;
-        padding: 2%;
-        font-size: 1.6rem;
-        font-weight: 500;
-        color: #ffffff;
-        border: 1px solid grey;
-        border-left-style: hidden;
-        border-right-style: hidden;
-        border-bottom-style: hidden;
-        /* width: 100%; */
-        /* margin-left: 8%; */
-        /* padding-bottom: 8px; */
-        /* margin-bottom: 8px; */
-        
-    }
-// styling users-setting-card
-    .users-card-wrapper{
-        display: flex;
-        flex-wrap: wrap;
-        width: 98%;
-        padding: 3%;
-        border-top-style: hidden;
-        /* border: 1px solid grey; */
-    }
-   
+    button {
+      padding: 10px 24px;
+      background: linear-gradient(to right, #0084ff, #0099ff);
+      border: none;
+      border-radius: 3px;
+      font-family: 'Nunito', sans-serif;
+      font-size: 0.875rem;
+      color: #ffffff;
+      cursor: pointer;
+      transition: 0.25s;
 
-    .display-name {
-        display: flex; 
-        border: none;
-        margin-left: 4%;
-        .left-section {
-            p {
-                font-size: 1rem;
-                font-weight: 500;
-                color: #ffffff;
+      :hover {
+        opacity: 0.5;
+      }
+    }
+  }
 
-                :first-child {
-                    margin-bottom: 4px;
-                    font-weight: 600;
-                }
-            }
-        }
-        button{
-            padding: 12px 26px;
-            margin-top: 16px;
-            background: linear-gradient(to right, #000000, #212121, #424949);
-            font-family: "Nunito", sans-serif;
-            font-size: 1rem;
-            font-weight: 600;
-            color: #ffffff;
-            cursor: pointer;
-            transition: 0.25s;
-            border: 1px solid grey;
-            height: 50px;
-            /* border-bottom-style: none; */
-            /* /* box-shadow: 3px 3px 6px #888888; */
-             :hover {
-              opacity: 0.5;
-            }
-          }
+  .track {
+    margin-bottom: 32px;
+    background: none;
+    border: none;
+    font-family: 'Nunito', sans-serif;
+    font-size: 1rem;
+    font-weight: 500;
+    color: #ffffff;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+
+    i {
+      margin-left: 4px;
+      font-size: 0.75rem;
+      transition: 0.25s;
     }
 
-    .update {
-        padding: 16px;
-        margin-bottom: 16px;
-        background-color: #2c2f33;
-        border-radius: 3px;
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
+    :hover {
+      i {
+        margin-left: 8px;
+      }
+    }
+  }
 
-        label {
-            margin-bottom: 4px;
-            font-size: 1rem;
-            font-weight: 500;
-            color: #ffffff;
-        }
+  .deactivate {
+    background: none;
+    border: none;
+    font-family: 'Nunito', sans-serif;
+    font-size: 1rem;
+    font-weight: 500;
+    color: #db4437;
+    cursor: pointer;
+    transition: 0.25s;
 
-        input {
-            width: 256px;
-            padding: 10px;
-            margin-bottom: 16px;
-            background-color: #23272a;
-            border: none;
-            border-radius: 3px;
-            font-family: 'Nunito', sans-serif;
-            font-size: 1rem;
-            font-weight: 500;
-            color: #ffffff;
-
-            ::placeholder {
-                color: dimgray;
-            }
-        }
-
-        button {
-            padding: 10px 24px;
-            background: linear-gradient(to right, #0084ff, #0099ff);
-            border: none;
-            border-radius: 3px;
-            font-family: 'Nunito', sans-serif;
-            font-size: 0.875rem;
-            color: #ffffff;
-            cursor: pointer;
-            transition: 0.25s;
-
-            :hover {
-                opacity: 0.5;
-            }
-        }
+    i {
+      margin-right: 8px;
     }
 
-    .track {
-        margin-bottom: 32px;
-        background: none;
-        border: none;
-        font-family: 'Nunito', sans-serif;
-        font-size: 1rem;
-        font-weight: 500;
-        color: #ffffff;
+    :hover {
+      opacity: 0.5;
+    }
+  }
+
+  .confirm {
+    margin-top: 16px;
+
+    p {
+      margin-bottom: 16px;
+      font-size: 1rem;
+      font-weight: 500;
+      color: #ffffff;
+    }
+
+    button {
+      padding: 10px 24px;
+      background: linear-gradient(to right, #0084ff, #0099ff);
+      border: none;
+      border-radius: 3px;
+      font-family: 'Nunito', sans-serif;
+      font-size: 0.875rem;
+      font-weight: 600;
+      color: #ffffff;
+      transition: 0.25s;
+
+      :first-of-type {
+        margin-right: 8px;
+        background: #2c2f33;
+        color: dimgray;
+      }
+
+      :last-child {
         cursor: pointer;
-        display: flex;
-        align-items: center;
-        
-        i {
-            margin-left: 4px;
-            font-size: 0.75rem;
-            transition: 0.25s;
-        }
 
         :hover {
-            i {
-                margin-left: 8px;
-            }
+          opacity: 0.5;
         }
+      }
     }
+  }
 
-    .deactivate {
-        background: none;
-        border: none;
-        font-family: 'Nunito', sans-serif;
-        font-size: 1rem;
-        font-weight: 500;
-        color: #db4437;
-        cursor: pointer;
-        transition: 0.25s;
+  @media (min-width: 768px) {
+    width: 95%;
+  }
 
-        i {
-            margin-right: 8px;
-        }
-
-        :hover {
-            opacity: 0.5;
-        }
-    }
-
-    .confirm {
-        margin-top: 16px;
-
-        p {
-            margin-bottom: 16px;
-            font-size: 1rem;
-            font-weight: 500;
-            color: #ffffff;
-        }
-
-        button {
-            padding: 10px 24px;
-            background: linear-gradient(to right, #0084ff, #0099ff);
-            border: none;
-            border-radius: 3px;
-            font-family: 'Nunito', sans-serif;
-            font-size: 0.875rem;
-            font-weight: 600;
-            color: #ffffff;
-            transition: 0.25s;
-
-            :first-of-type {
-                margin-right: 8px;
-                background: #2c2f33;
-                color: dimgray;
-            }
-
-            :last-child {
-                cursor: pointer;
-
-                :hover {
-                    opacity: 0.5;
-                }
-            }
-        }
-    }
-
-    @media (min-width: 768px) {
-        width: 95%;
-    }
-
-    @media (min-width: 1024px) {
-        width: 972.8px;
-    }
+  @media (min-width: 1024px) {
+    width: 972.8px;
+  }
 `;
 
 export default SettingsContainer;
