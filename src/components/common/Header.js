32f325ad992224c520d9ba1242097d4c08da2b2b
Ian Luchini <iancarloluchini@gmail.com>
2021-03-15 18:18:43 -0400

    clean up and refactor

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -7,7 +7,7 @@ import AlumniLogo from'../../img/AlumniLogo.svg';
 import burgerMenu from '../../img/burgerMenu.svg';
 
 const Header = props => {
-  const [hamburgerMenu, setHamburgerMenu] = useState(false);
+  const [profileDropdown, setProfileDropdown] = useState(false);
   const [viewModeDropdown, setViewModeDropdown] = useState(false)
   const [searchInput, setSearchInput] = useState('');
   useEffect(() => {
@@ -27,29 +27,30 @@ const Header = props => {
     props.history.push('/full-search');
   };
 
+  // Checks for click on window and sets dropdowns to false if the click is not on either one of the dropdowns
   window.addEventListener('click', e => {
     if (!e.srcElement.alt) {
       setViewModeDropdown(false);
-      setHamburgerMenu(false);
+      setProfileDropdown(false);
     }
   });
 
-  const viewModeDropdownHandler = e => {
-    setViewModeDropdown(!viewModeDropdown);
-       if (hamburgerMenu) 
-         setHamburgerMenu(false);
-  }
-
-  const profileDropdownHandler = e => {
-    setHamburgerMenu(!hamburgerMenu);
+  const DropdrownHandler = e => {
+    if (e.target.id === 'viewModeDropdown') {
+      setViewModeDropdown(!viewModeDropdown);
+       if (profileDropdown) 
+         setProfileDropdown(false);  
+    } else if (e.target.id === 'profileDropdown') {
+      setProfileDropdown(!profileDropdown);
        if (viewModeDropdown) 
-         setViewModeDropdown(false);
+         setViewModeDropdown(false);  
+    }
   }
   
   return (
     <HeaderContainer>
       <div className='logo' onClick={() => {
-        setHamburgerMenu(false);
+        setProfileDropdown(false);
         props.history.push('/');
       }}>
         <img src={AlumniLogo} alt='Lambda School logo' />
@@ -60,20 +61,14 @@ const Header = props => {
         <button type='submit'><i className='fas fa-search'></i></button>
       </form>
 
-      <img src={burgerMenu} alt='dropdown menu' width="20px" onClick={viewModeDropdownHandler}/>
+      <img src={burgerMenu} alt='dropdown menu' width="20px" id='viewModeDropdown' onClick={DropdrownHandler}/>
       
-      <img className='profile-picture' src={props.user.profilePicture} alt='profile icon' onClick={profileDropdownHandler}/>
+      <img className='profile-picture' src={props.user.profilePicture} alt='profile icon' id='profileDropdown' onClick={DropdrownHandler}/>
 
-      {hamburgerMenu && <div className='dropdown'>
+      {profileDropdown && <div className='dropdown'>
         <p onClick={() => props.history.push('/faq')}><i className='fas fa-question'></i>FAQ</p>
         <p onClick={() => props.history.push(`/user/${props.user.id}`)}><i className='fas fa-user'></i>My Profile</p>
         <p onClick={() => props.history.push('/settings')}><i className='fas fa-cog'></i>Settings</p>
-        {/* {props.user.role_id === 3 ? (
-          <p onClick={() => props.history.push('/admin-settings')}><img src={adminLogo} id='admin' />Admin Settings</p>
-        ) : ''}
-        {props.user.role_id > 1 ? (
-          <p onClick={() => props.history.push('/mod-settings')}><img src={adminLogo} id='mod' />Moderator Settings</p>
-        ) : ''} */}
         <p onClick={() => props.logOut(props.history)}><i className='fas fa-sign-out-alt'></i>Log Out</p>
       </div>}
 
