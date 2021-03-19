487a886715c84c1f067a7ae413de69ed0369cb74
Ian Luchini <iancarloluchini@gmail.com>
2021-03-19 19:33:36 -0400

    added functionality to the avatar dropdown

--- src/assets/SampleStyles.js
+++ src/assets/SampleStyles.js
@@ -1,12 +1,17 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { 
-  fetchRooms,
-  fetchRecent,
-  fetchUser
-} from '../store/actions';
-import { Layout, Menu, Breadcrumb, Input, Row, Col, Avatar } from 'antd';
+import { fetchRooms, fetchRecent, fetchUser } from '../store/actions';
+import {
+  Layout,
+  Menu,
+  Breadcrumb,
+  Input,
+  Row,
+  Col,
+  Avatar,
+  Dropdown,
+} from 'antd';
 import {
   UserOutlined,
   ShopFilled,
@@ -17,8 +22,6 @@ import {
 } from '@ant-design/icons';
 import AlumniLogo from '../img/AlumniLogo.svg';
 
-
-
 const SampleStyles = (props) => {
   const { SubMenu, Item } = Menu;
   const { Header, Content, Sider } = Layout;
@@ -27,29 +30,78 @@ const SampleStyles = (props) => {
   useEffect(() => {
     props.fetchRooms();
     // props.fetchRecent(); // This is throwing an internal server error
-    if (Object.keys(props.user).length === 0) { 
+    if (Object.keys(props.user).length === 0) {
       props.fetchUser();
-    };
+    }
   }, []);
 
   return (
     <Layout>
       <Header className="header" style={{ width: '100%' }}>
         {/* <div className="logo" /> */}
-        <Row style={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'space-between'}}>
-          <Col span={6} style={{ display: 'flex', justifyContent: 'flex-start'}}>
-            <div style={{ width: '60%'}}onClick={() => {
-              props.history.push('/');
-            }}>
-              <img src={AlumniLogo} width='100%'/>
+        <Row
+          style={{
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            justifyContent: 'space-between',
+          }}
+        >
+          <Col
+            span={6}
+            style={{ display: 'flex', justifyContent: 'flex-start' }}
+          >
+            <div
+              style={{ width: '60%' }}
+              onClick={() => {
+                props.history.push('/');
+              }}
+            >
+              <img src={AlumniLogo} width="100%" />
             </div>
           </Col>
-          <Col span={6} style={{ display: 'flex', flexFlow: 'column nowrap', justifyContent: 'center'}}>
-            <Search placeholder="input search text" onSearch={e => console.log('search implementation pending')} enterButton />
+          <Col
+            span={6}
+            style={{
+              display: 'flex',
+              flexFlow: 'column nowrap',
+              justifyContent: 'center',
+            }}
+          >
+            <Search
+              placeholder="input search text"
+              onSearch={(e) => console.log('search implementation pending')}
+              enterButton
+            />
           </Col>
-          <Col span={6} style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center'}} >
-            <Avatar src={props.user.profilePicture} size="large"/>
-            {/* <img src={props.user.profilePicture} alt='user avatar' /> */}
+          <Col
+            span={6}
+            style={{
+              display: 'flex',
+              justifyContent: 'flex-end',
+              alignItems: 'center',
+            }}
+          >
+            <Dropdown
+              placement="bottomRight"
+              arrow
+              trigger={['click']}
+              overlay={
+                <Menu>
+                  <Item>
+                    <Link to='/faq'>FAQ</Link>
+                  </Item>
+                  <Item>
+                    <Link to={`/user/${props.user.id}`}>My Profile</Link>
+                  </Item>
+                  <Item>
+                    <Link to='/settings'>Settings</Link>
+                  </Item>
+                </Menu>
+              }
+            >
+              <Avatar src={props.user.profilePicture} size="large" />
+            </Dropdown>
           </Col>
         </Row>
         {/* <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
@@ -113,9 +165,10 @@ const mapStateToProps = (state) => {
   return {
     user: state.user,
     rooms: state.rooms,
-    posts: state.posts
+    posts: state.posts,
   };
 };
 
-
-export default connect(mapStateToProps, { fetchRooms, fetchRecent, fetchUser })(SampleStyles);
+export default connect(mapStateToProps, { fetchRooms, fetchRecent, fetchUser })(
+  SampleStyles
+);
