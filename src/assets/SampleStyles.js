7338b577656c07c3b361afa3cd1dbee5e065ca4e
Ian Luchini <iancarloluchini@gmail.com>
2021-03-19 19:14:00 -0400

    New dashboard header components in place and inline styled

--- src/assets/SampleStyles.js
+++ src/assets/SampleStyles.js
@@ -1,8 +1,12 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { fetchRooms } from '../store/actions';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { 
+  fetchRooms,
+  fetchRecent,
+  fetchUser
+} from '../store/actions';
+import { Layout, Menu, Breadcrumb, Input, Row, Col, Avatar } from 'antd';
 import {
   UserOutlined,
   ShopFilled,
@@ -11,27 +15,50 @@ import {
   ShopOutlined,
   NotificationOutlined,
 } from '@ant-design/icons';
+import AlumniLogo from '../img/AlumniLogo.svg';
 
 
 
 const SampleStyles = (props) => {
   const { SubMenu, Item } = Menu;
   const { Header, Content, Sider } = Layout;
+  const { Search } = Input;
 
   useEffect(() => {
     props.fetchRooms();
-    console.log(props);
+    // props.fetchRecent(); // This is throwing an internal server error
+    if (Object.keys(props.user).length === 0) { 
+      props.fetchUser();
+    };
   }, []);
 
   return (
     <Layout>
-      <Header className="header">
+      <Header className="header" style={{ width: '100%' }}>
         {/* <div className="logo" /> */}
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+        <Row style={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'space-between'}}>
+          <Col span={6} style={{ display: 'flex', justifyContent: 'flex-start'}}>
+            <div style={{ width: '60%'}}onClick={() => {
+              props.history.push('/');
+            }}>
+              <img src={AlumniLogo} width='100%'/>
+            </div>
+          </Col>
+          <Col span={6} style={{ display: 'flex', flexFlow: 'column nowrap', justifyContent: 'center'}}>
+            <Search placeholder="input search text" onSearch={e => console.log('search implementation pending')} enterButton />
+          </Col>
+          <Col span={6} style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center'}} >
+            <Avatar src={props.user.profilePicture} size="large"/>
+            {/* <img src={props.user.profilePicture} alt='user avatar' /> */}
+          </Col>
+        </Row>
+        {/* <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
           <Item key="1">nav 1</Item>
-          <Item key="2">nav 2</Item>
+          <Item key="2">
+            <Search placeholder="input search text" onSearch={e => console.log(e.value)} enterButton />
+          </Item>
           <Item key="3">nav 3</Item>
-        </Menu>
+        </Menu> */}
       </Header>
       <Layout>
         <Sider width={200} className="site-layout-background">
@@ -57,9 +84,6 @@ const SampleStyles = (props) => {
                   </Item>
                 );
               })}
-              {/* <Item key="6">option6</Item>
-              <Item key="7">option7</Item>
-              <Item key="8">option8</Item> */}
             </SubMenu>
           </Menu>
         </Sider>
@@ -87,9 +111,11 @@ const SampleStyles = (props) => {
 
 const mapStateToProps = (state) => {
   return {
-    rooms: state.rooms
+    user: state.user,
+    rooms: state.rooms,
+    posts: state.posts
   };
 };
 
 
-export default connect(mapStateToProps, { fetchRooms })(SampleStyles);
+export default connect(mapStateToProps, { fetchRooms, fetchRecent, fetchUser })(SampleStyles);
