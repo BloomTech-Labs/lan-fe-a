0658cf338fe0c90109791c9dabebdb6cbf58986d
Ian Luchini <iancarloluchini@gmail.com>
2021-03-22 19:00:43 -0400

    adding functionality to create room feature

--- src/assets/SiderMenu.js
+++ src/assets/SiderMenu.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-
-import { Menu } from 'antd';
-import { HeartOutlined, ShopOutlined, PlusSquareOutlined } from '@ant-design/icons';
+import { createRoom, fetchRooms } from '../store/actions/index';
+import { Menu, Modal, Form, Input, Button } from 'antd';
+import {
+  HeartOutlined,
+  ShopOutlined,
+  PlusSquareOutlined,
+} from '@ant-design/icons';
 
 const SiderMenu = (props) => {
+  const [newRoomModalVisible, setNewRoomModalVisible] = useState(false);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  const inputHandler = (prevValues, curValues) => {
+    setTitle(curValues.title);
+    setDescription(curValues.description);
+  };
+
   return (
     <Menu
       mode={localStorage.getItem('menuMode') || 'inline'}
@@ -31,17 +44,62 @@ const SiderMenu = (props) => {
             </Menu.Item>
           );
         })}
-        <Menu.Item icon={<PlusSquareOutlined />}>Create Room</Menu.Item>
+        <Menu.Item
+          onClick={() => setNewRoomModalVisible(true)}
+          icon={<PlusSquareOutlined />}
+        >
+          Create Room
+        </Menu.Item>
       </Menu.SubMenu>
+      <Modal
+        title="Create Room"
+        centered
+        visible={newRoomModalVisible}
+        okButtonProps={{ htmlType: 'submit' }}
+        onOk={() => {
+          props
+            .createRoom({ room_name: title, description: description })
+            .then(() => {
+              setTitle('');
+              setDescription('');
+              props.fetchRooms();
+            })
+            .catch(() => {
+              console.log('failed to create room');
+            });
+          console.log(title, description);
+          setNewRoomModalVisible(false);
+        }}
+        onCancel={() => setNewRoomModalVisible(false)}
+      >
+        <Form>
+          <Form.Item
+            name="title"
+            rules={[{ required: true, message: 'Title required' }]}
+            shouldUpdate={inputHandler}
+          >
+            <Input placeholder="Title" />
+          </Form.Item>
+          <Form.Item
+            name="description"
+            rules={[{ required: true, message: 'Description required' }]}
+            shouldUpdate={inputHandler}
+          >
+            <Input.TextArea placeholder="Description" />
+          </Form.Item>
+          {/* <Form.Item>
+            <Button /> 
+          </Form.Item> */}
+        </Form>
+      </Modal>
     </Menu>
   );
 };
 
-
 const mapStateToProps = (state) => {
   return {
-    rooms: state.rooms
+    rooms: state.rooms,
   };
 };
 
-export default connect(mapStateToProps)(SiderMenu);
+export default connect(mapStateToProps, { createRoom, fetchRooms })(SiderMenu);
