272d187e68d12724beb43d5d4b705d1bc8a3b4c7
Matt Canavan <mattcanavan@users.noreply.github.com>
2021-03-26 16:28:30 -0700

    Fixed merge conflict.

--- src/components/post/Comment.js
+++ src/components/post/Comment.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
 import { connect } from 'react-redux';
 import {
   likeComment,
@@ -8,14 +7,21 @@ import {
   fetchPostCommentsByRecent,
   flagComment,
 } from '../../store/actions';
+import { toggleDropdown } from '../../utils/toggleDropdown';
 import moment from 'moment';
 import CommentContainer from './styles/commentStyle.js';
+import StyledModel from './styles/flagModelStyle';
 
 const Comment = (props) => {
   const [liked, setLiked] = useState(false);
   const [likes, setLikes] = useState(0);
   const [editing, setEditing] = useState(false);
+  const [modelIsOpen, setModelIsOpen] = useState(false);
   const [moreOptions, setMoreOptions] = useState(false);
+  const [note, setNote] = useState('');
+  const [reason, setReason] = useState('');
+
+  const { comment } = props;
   useEffect(() => setLikes(props.comment.likes), [props.comment]);
 
   useEffect(() => {
@@ -30,18 +36,16 @@ const Comment = (props) => {
     }
   }, [props.usersLikedComments]);
 
-  // likes comment
-  const like = () => {
-    setLiked(true);
-    setLikes(likes + 1);
-    props.likeComment(props.comment.id);
-  };
-
-  // removes like from comment
-  const unlike = () => {
-    setLiked(false);
-    setLikes(likes - 1);
-    props.unlikeComment(props.comment.id);
+  const onClickCommentLikesHandler = () => {
+    if (!liked) {
+      setLikes(likes + 1);
+      setLiked(!liked);
+      props.likeComment(props.comment.id);
+    } else {
+      setLikes(likes - 1);
+      setLiked(!liked);
+      props.unlikeComment(props.comment.id);
+    }
   };
 
   //removes a comment by UserId
@@ -56,9 +60,35 @@ const Comment = (props) => {
       });
   };
 
-  const handleFlaggingComment = () => {
-    props.flagComment(props.comment.id);
-    setMoreOptions(!moreOptions);
+  //flags comment
+  const handleFlaggingComment = (reason) => {
+    props.flagComment(props.comment.id, reason);
+  };
+
+  //opens flagging model
+  const openModel = () => {
+    setModelIsOpen(true);
+  };
+
+  //closes flagging model
+  const closeModel = () => {
+    setModelIsOpen(false);
+  };
+
+  //flags comment and closes model
+  const handleFlagModel = (reason) => {
+    handleFlaggingComment(reason);
+    closeModel();
+  };
+
+  //Handles note input on model
+  const handleNoteChange = (evt) => {
+    setNote(evt.target.value);
+  };
+
+  //Sets reason for flagging
+  const handleSetReason = (reason) => {
+    setReason(reason);
   };
 
   return (
@@ -86,49 +116,118 @@ const Comment = (props) => {
         <p className="likes">
           {liked ? (
             <>
-              <i className="blue fas fa-chevron-up" onClick={unlike}></i>
-              <span className='blue'>{likes}</span>
+              <i
+                className="blue fas fa-chevron-up"
+                onClick={onClickCommentLikesHandler}
+              ></i>
+              <span className="blue">{likes}</span>
             </>
           ) : (
             <>
-              <i className="white fas fa-chevron-up" onClick={like}></i>
-              <span className='white'>{likes}</span>
+              <i
+                className="white fas fa-chevron-up"
+                onClick={onClickCommentLikesHandler}
+              ></i>
+              <span className="white">{likes}</span>
             </>
           )}
         </p>
-        <div
-          className="more-options"
-          onClick={() => {
-            setMoreOptions(!moreOptions);
-            console.log('clicked');
-          }}
-        >
+        {/* Comment dropdown menu */}
+        <div className="dropdown-menu" onClick={toggleDropdown}>
           <p className="fas fa-ellipsis-h"></p>
-          {moreOptions && (
-            <div className="commentdropdown">
-              {props.comment.user_id === props.user.id ? (
-                <button onClick={() => removeComments(props.comment.id)}>
-                  Delete Post
-                </button>
-              ) : (
-                ''
-              )}
-              {props.comment.user_id === props.user.id ? (
-                <button onClick={() => setEditing(true)}>Edit</button>
-              ) : (
-                ''
-              )}
-              {props.comment.user_id !== props.user.id ? (
-                <button className="flag-button" onClick={handleFlaggingComment}>
-                  Flag Post
-                </button>
-              ) : (
-                ''
-              )}
-            </div>
-          )}
+          <div className="dropdown-content hidden">
+            {props.comment.user_id === props.user.id ? (
+              <button onClick={() => removeComments(props.comment.id)}>
+                Delete Post
+              </button>
+            ) : (
+              ''
+            )}
+            {props.comment.user_id === props.user.id ? (
+              <button onClick={() => setEditing(true)}>Edit</button>
+            ) : (
+              ''
+            )}
+            {props.comment.user_id !== props.user.id ? (
+              <button className="flag-button" onClick={openModel}>
+                Flag Comment
+              </button>
+            ) : (
+              ''
+            )}
+          </div>
         </div>
       </div>
+      {modelIsOpen && (
+        <StyledModel
+          isOpen={modelIsOpen}
+          onRequestClose={closeModel}
+          contentLabel="Flag Comment"
+          ariaHideApp={false} //Hides from screen readers. This isn't ideal, but without it, it throws an error
+        >
+          <div className="fpm">
+            <div className="fpm-top">
+              <div>
+                <button onClick={closeModel}>Back</button>
+              </div>
+              <h1>Reason for Flagging</h1>
+            </div>
+            <div className="fpm-bottom">
+              <div>
+                <button onClick={() => handleSetReason('Spam')}>Spam</button>
+              </div>
+              <div>
+                <button
+                  onClick={() => handleSetReason('Bullying or Harrassment')}
+                >
+                  Bullying or Harrassment
+                </button>
+              </div>
+              <div>
+                <button
+                  onClick={() => handleSetReason('Hate Speach or Symbols')}
+                >
+                  Hate Speach or Symbols
+                </button>
+              </div>
+              <div>
+                <button
+                  onClick={() => handleSetReason('Nudity or Sexual Content')}
+                >
+                  Nudity or Sexual Content
+                </button>
+              </div>
+              <div>
+                <button onClick={() => handleSetReason('I just dislike it')}>
+                  I just dislike it
+                </button>
+              </div>
+              <div>
+                <button onClick={() => handleSetReason('Other')}>Other</button>
+              </div>
+              <div>
+                <p>Selected reason: {reason}</p>
+              </div>
+              <div>
+                <div>
+                  <form>
+                    <p>Note</p>
+                    <input
+                      type="text"
+                      name="note"
+                      value={note}
+                      onChange={handleNoteChange}
+                    />
+                  </form>
+                </div>
+              </div>
+              <div>
+                <button onClick={() => handleFlagModel(reason)}>Submit</button>
+              </div>
+            </div>
+          </div>
+        </StyledModel>
+      )}
     </CommentContainer>
   );
 };
