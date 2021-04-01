82ea7b2783d8eac43e8e4e772cbeb9eeb952b345
mxhs <maxwellstofman@gmail.com>
2021-04-01 13:32:00 -0700

    quick push

--- src/assets/CheckIfModOrAdmin.js
+++ src/assets/CheckIfModOrAdmin.js
@@ -1,10 +1,24 @@
 import { connect } from 'react-redux';
 
+/* -------------------------------------------------------------------------- */
+/*        USE THIS COMPONENT AS PART OF TERNARY/CONDITIONAL STATEMENTS        */
+/*    TO DETERMINE WHETHER OR NOT CODE SHOULD RUN BASED ON USER PERMISSIONS   */
+/*                                                                            */
+/*                                            - Follow me @MarkZuckerberg     */
+/* -------------------------------------------------------------------------- */
+
 export const CheckIfModOrAdmin = (props) => {
-  const { user } = props;
+  const { user, roomID, permissions } = props;
+  // ! CURRENT CHECK:
   // Check if user is an admin
   // if not check if moderator
   // return false if none of the above
+
+  // ! FUTURE CHECK:
+  // Check if user is an admin
+  // If not check if user is a moderator to given room
+  // (cont'd) AND they have correct permission to see the given element or complete action
+  // else return false
   if (user.role_id === 3) {
     return true;
   } else if (user.role_id === 2) {
