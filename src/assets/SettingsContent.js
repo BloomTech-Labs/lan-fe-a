04c461ef2e004e7c1f11da59b105df7c1668ac7f
Ismail Al Kamal <ismail@nawatt.com>
2021-04-13 01:11:23 +0300

    settings done

--- src/assets/SettingsContent.js
+++ src/assets/SettingsContent.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
-import { Button, Card } from 'antd';
-import { CloseCircleOutlined } from '@ant-design/icons';
+import { Button, Card, Input, Dropdown, Menu } from 'antd';
+import { CloseCircleOutlined, DownOutlined } from '@ant-design/icons';
+import { updateUserDisplayName, setTrackSettings } from '../store/actions';
 
 /* eslint-disable no-undef */
 const BACKEND_URL =
@@ -10,8 +12,33 @@ const BACKEND_URL =
 
 const SettingsContent = (props) => {
   const [deactivate, setDeactivate] = useState(false);
-
   const history = useHistory();
+
+  const initialSettings = {
+    displayName: props.user.displayName,
+    track: props.user.track,
+  };
+  const [settings, setSettings] = useState(initialSettings);
+
+  const initialEditSettings = {
+    displayName: false,
+    track: false,
+  };
+  const [editSettings, setEditSettings] = useState(initialEditSettings);
+
+  const [input, setInput] = useState('');
+
+  useEffect(() => {
+    setSettings({ ...settings, displayName: props.user.displayName });
+  }, [props.user]);
+
+  useEffect(() => {
+    if (props.actKey == 'Settings') {
+      setEditSettings(initialEditSettings);
+      setInput('');
+      setSettings(initialSettings);
+    }
+  }, [props.actKey]);
 
   // deletes user from users table
   const deleteHandler = () => {
@@ -27,22 +54,146 @@ const SettingsContent = (props) => {
       });
   };
 
+  const onChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (editSettings.displayName) {
+      props.updateUserDisplayName(props.user, input);
+      setSettings({ ...settings, displayName: input });
+      setInput('');
+      setEditSettings({ ...editSettings, displayName: false });
+    } else if (editSettings.track) {
+      props.setTrackSettings(props.user, settings.track);
+      setEditSettings({ ...editSettings, track: false });
+    }
+  };
+
+  function handleMenuClick(e) {
+    // message.info('Click on menu item.');
+    setSettings({ ...settings, track: e.key });
+  }
+
+  const menu = (
+    <Menu onClick={handleMenuClick}>
+      <Menu.Item key="WEB">WEB</Menu.Item>
+      <Menu.Item key="DS">DS</Menu.Item>
+      <Menu.Item key="iOS">iOS</Menu.Item>
+      <Menu.Item key="UX">UX</Menu.Item>
+      <Menu.Item key="AND">AND</Menu.Item>
+      <Menu.Item key="None">None</Menu.Item>
+    </Menu>
+  );
+
   return (
     <Card title="Account" style={{ width: 500 }}>
       <Card
         type="inner"
         title="Display Name"
-        extra={<Button type="link">Change</Button>}
+        extra={
+          <Button
+            type="link"
+            onClick={() =>
+              setEditSettings({ ...editSettings, displayName: true })
+            }
+          >
+            Change
+          </Button>
+        }
       >
-        {props.user.display_name}
+        {settings.displayName}
+        {editSettings.displayName && (
+          <form
+            style={{ marginTop: '10px' }}
+            autoComplete="off"
+            spellCheck="false"
+            onSubmit={onSubmit}
+          >
+            <label htmlFor="display-name">
+              Set a new display name (Max 15 characters)
+            </label>
+            <Input
+              name="display-name"
+              type="text"
+              placeholder="Enter new display name"
+              value={input}
+              style={{ marginTop: '10px' }}
+              onChange={onChange}
+              maxLength="15"
+              required
+            />
+            <Button
+              style={{ marginTop: '10px' }}
+              type="primary"
+              htmlType="submit"
+            >
+              Submit
+            </Button>
+            <Button
+              type="danger"
+              style={{ marginLeft: '10px' }}
+              onClick={() => {
+                setInput('');
+                setEditSettings({ ...editSettings, displayName: false });
+              }}
+            >
+              Cancel
+            </Button>
+          </form>
+        )}
       </Card>
       <Card
         style={{ marginTop: 16 }}
         type="inner"
         title="Lambda School Track"
-        extra={<Button type="link">Change</Button>}
+        extra={
+          <Button
+            type="link"
+            onClick={() => setEditSettings({ ...editSettings, track: true })}
+          >
+            Change
+          </Button>
+        }
       >
         {props.user.track.toUpperCase()}
+        {editSettings.track && (
+          <form
+            style={{ marginTop: '10px' }}
+            autoComplete="off"
+            spellCheck="false"
+            onSubmit={onSubmit}
+          >
+            <label htmlFor="display-name" style={{ marginRight: '10px' }}>
+              Change Lambda School Track:
+            </label>
+            <Dropdown overlay={menu}>
+              <Button>
+                {settings.track} <DownOutlined />
+              </Button>
+            </Dropdown>
+            <div>
+              <Button
+                style={{ marginTop: '10px' }}
+                type="primary"
+                htmlType="submit"
+              >
+                Submit
+              </Button>
+              <Button
+                type="danger"
+                style={{ marginLeft: '10px' }}
+                onClick={() => {
+                  setSettings(initialSettings);
+                  setEditSettings({ ...editSettings, track: false });
+                }}
+              >
+                Cancel
+              </Button>
+            </div>
+          </form>
+        )}
       </Card>
       <Button
         type="primary"
@@ -64,4 +215,13 @@ const SettingsContent = (props) => {
   );
 };
 
-export default SettingsContent;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+  };
+};
+
+export default connect(mapStateToProps, {
+  updateUserDisplayName,
+  setTrackSettings,
+})(SettingsContent);
