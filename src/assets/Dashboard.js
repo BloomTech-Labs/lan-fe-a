cc2f3d390be13380e8a62c62e8329f1e10cfdd7b
Ismail Al Kamal <ismail@nawatt.com>
2021-04-12 19:30:51 +0300

    removed breadcrumbs

--- src/assets/Dashboard.js
+++ src/assets/Dashboard.js
@@ -9,7 +9,7 @@ import {
   setSearch,
   fetchFlagReasons,
 } from '../store/actions';
-import { Layout, Breadcrumb } from 'antd';
+import { Layout } from 'antd';
 
 import { PrivateRoute } from '../utils/privateRoute';
 import Navbar from './Navbar';
@@ -50,11 +50,6 @@ const Dashboard = (props) => {
             <SiderMenu />
           </Sider>
           <Layout style={{ padding: '0 24px 24px' }}>
-            <Breadcrumb style={{ margin: '16px 0' }}>
-              <Breadcrumb.Item>Home</Breadcrumb.Item>
-              <Breadcrumb.Item>List</Breadcrumb.Item>
-              <Breadcrumb.Item>App</Breadcrumb.Item>
-            </Breadcrumb>
             <Content
               className="site-layout-background"
               style={{
