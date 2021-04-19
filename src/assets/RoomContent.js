43391aa71f42a345e47b83ff989bdd769b12cf69
Peter <ploflan@gmail.com>
2021-04-19 15:52:54 -0400

    In RoomContent, changed showModal to showNewCardModal for clarity

--- src/assets/RoomContent.js
+++ src/assets/RoomContent.js
@@ -26,7 +26,7 @@ const RoomContent = (props) => {
   const [confirmLoading, setConfirmLoading] = useState(false);
 
   // modal funcs
-  const showModal = () => setVisible(true);
+  const showNewCardModal = () => setVisible(true);
 
   // TODO: this is not an ideal solution eventually we will need proper error handling
   // for posting when empty/double posting
@@ -107,7 +107,7 @@ const RoomContent = (props) => {
             </h2>
 
             {/* button to open modal */}
-            <Button type="primary" onClick={showModal}>
+            <Button type="primary" onClick={showNewCardModal}>
               New Discussion
             </Button>
 
