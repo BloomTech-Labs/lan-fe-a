f2dcf6d6dc72bb8432703b17fb2cf49b75bb6d32
Gabby <gabbyvicas@gmail.com>
2021-04-23 10:27:57 -0400

    changed the width of log in page, changed color of text, and changed the width of textbox as well as continue button

--- src/assets/OnboardingStyles.js
+++ src/assets/OnboardingStyles.js
@@ -1,46 +1,35 @@
 import styled from 'styled-components';
 
 const OnboardingContainer = styled.div`
-  padding: 10%;
-  margin: 0 auto;
+  width: 70%;
+  margin: auto;
   display: flex;
   flex-direction: column;
   justify-content: center;
-
-.title{
-  text-align: center;
-  margin-top: 15%;
-}
-  h1{
+  align-items: center;
+  h1 {
     margin-bottom: 4px;
-    font-size: 1.75rem;
+    font-size: 2rem;
     font-weight: 700;
-    color: #0e1111;
+    color: rgba(0,0,0,0.85);
   }
-
   .instructions {
     margin-bottom: 16px;
     font-size: 1rem;
     font-weight: 500;
-    color: 	#0e1111;
-    text-align: center;
+    color:rgba(0,0,0,0.65);
   }
-
   .tracks {
     display: flex;
+    align-content: flex-start;
     justify-content: center;
     flex-wrap: wrap;
-    margin-bottom: 5%;
-  }
-
+    width: 150px;
+    height:70px;
     .track {
-      display: flex;
-      align-items: center;
-      justify-content: center;
       margin-right: 8px;
       margin-bottom: 8px;
       padding: 10px 24px;
-      width: 100px;
       border: solid 1px #0099ff;
       border-radius: 3px;
       font-family: 'Nunito', sans-serif;
@@ -50,107 +39,85 @@ const OnboardingContainer = styled.div`
       transition: 0.25s;
       outline: none;
     }
-
     .track:nth-child(1) {
       background: ${(props) => (props.tracks[0].value ? '#0099ff' : 'none')};
       color: ${(props) => (props.tracks[0].value ? '#ffffff' : '#0099ff')};
-
       :hover {
         background-color: #0099ff;
         color: #ffffff;
       }
-
       :active {
         opacity: 0.5;
       }
     }
-
     .track:nth-child(2) {
       background: ${(props) => (props.tracks[1].value ? '#0099ff' : 'none')};
       color: ${(props) => (props.tracks[1].value ? '#ffffff' : '#0099ff')};
-
       :hover {
         background-color: #0099ff;
         color: #ffffff;
       }
-
       :active {
         opacity: 0.5;
       }
     }
-
     .track:nth-child(3) {
       background: ${(props) => (props.tracks[2].value ? '#0099ff' : 'none')};
       color: ${(props) => (props.tracks[2].value ? '#ffffff' : '#0099ff')};
-
       :hover {
         background-color: #0099ff;
         color: #ffffff;
       }
-
       :active {
         opacity: 0.5;
       }
     }
-
     .track:nth-child(4) {
       background: ${(props) => (props.tracks[3].value ? '#0099ff' : 'none')};
       color: ${(props) => (props.tracks[3].value ? '#ffffff' : '#0099ff')};
-
       :hover {
         background-color: #0099ff;
         color: #ffffff;
       }
-
       :active {
         opacity: 0.5;
       }
     }
-
     .track:nth-child(5) {
       background: ${(props) => (props.tracks[4].value ? '#0099ff' : 'none')};
       color: ${(props) => (props.tracks[4].value ? '#ffffff' : '#0099ff')};
-
       :hover {
         background-color: #0099ff;
         color: #ffffff;
       }
-
       :active {
         opacity: 0.5;
       }
     }
-
     .track:nth-child(6) {
       background: ${(props) => (props.tracks[5].value ? '#0099ff' : 'none')};
       color: ${(props) => (props.tracks[5].value ? '#ffffff' : '#0099ff')};
-
       :hover {
         background-color: #0099ff;
         color: #ffffff;
       }
-
       :active {
         opacity: 0.5;
       }
     }
-  
-
+  }
   .continue {
     width: 100%;
     display: flex;
     justify-content: center;
     margin-top: 1rem;
-
     .error {
       font-size: 0.875rem;
       font-weight: 600;
       color: #ea4335;
     }
-
     button {
-      padding: 15px 30px;
-      width: 50%;
+      padding: 10px 24px;
       background: linear-gradient(to right, #0084ff, #0099ff);
       border: none;
       border-radius: 3px;
@@ -160,36 +127,24 @@ const OnboardingContainer = styled.div`
       color: #ffffff;
       cursor: pointer;
       transition: 0.25s;
-
+      width: 50%;
       i {
         margin-left: 4px;
         font-size: 0.75rem;
       }
-
       :hover {
         opacity: 0.5;
       }
     }
   }
-  
   .gitHubUserName{
     border-radius: 3px;
     border-color: #0099ff;
     width: 100%;
   }
-  
-
-  .github{
-    display: flex;
-    flex-direction: column;
-    align-items:center;
-    margin-bottom: 10%;
-  }
-
   @media (min-width: 375px) {
     height: 90vh;
   }
-
   @media (min-width: 768px) {
     width: 691.2px;
   }
